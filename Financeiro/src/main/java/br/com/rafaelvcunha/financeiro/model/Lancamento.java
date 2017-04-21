cd52807d08c2b28ae1cb9d6f41de6282ef691a85
rafaelvcunha <rafaelvcunha@gmail.com>
2017-04-20 21:53:53 -0300

    realizando os mappers da classe Lancamento

--- Financeiro/src/main/java/br/com/rafaelvcunha/financeiro/model/Lancamento.java
+++ Financeiro/src/main/java/br/com/rafaelvcunha/financeiro/model/Lancamento.java
@@ -4,17 +4,50 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "lancamento")
 public class Lancamento implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue	
 	private Long id;
+	
+	@ManyToOne( optional = false )
+	@JoinColumn( name = "pessoa_id" )
 	private Pessoa pessoa;
+	
+	@Column( length = 80, nullable = false )
 	private String descricao;
+	
+	@Column( precision = 10, scale = 2, nullable = false )	
 	private BigDecimal valor;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
 	private TipoLancamento tipo;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "data_vencimento", nullable = false)
 	private Date dataVencimento;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "data_pagamento", nullable = true)
 	private Date dataPagamento;
+	
 
 	public Long getId() {
 		return id;
@@ -22,18 +55,21 @@ public class Lancamento implements Serializable {
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
 	public Pessoa getPessoa() {
 		return pessoa;
 	}
 	public void setPessoa(Pessoa pessoa) {
 		this.pessoa = pessoa;
 	}
+	
 	public String getDescricao() {
 		return descricao;
 	}
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+	
 	public BigDecimal getValor() {
 		return valor;
 	}
